bcba98da447480f7a68ae2cb6bd944c5b31daba7
KonstPap <32640290+KonstPap@users.noreply.github.com>
2020-10-30 15:34:49 +0200

    Create coordinates.c

---
+++ coordinates.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+    double A[10],r,c,B[10],d;
+    int n,i;
+    printf("Give radius of a circle and the number of points\n");
+    scanf("%lf %d",&r,&n);
+    printf("Give coordinates for point %d\n",i+1);
+        scanf("%lf %lf",&c,&d);
+        A[i]=c;
+    return 0;
